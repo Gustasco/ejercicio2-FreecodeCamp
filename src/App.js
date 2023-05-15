0a62ba20bf0d53246856c40c1796fca5ffa08bbd
Gustavo_Espinoza <gustavo.espinoza.aliste@gmail.com>
2023-05-15 00:07:06 -0400

    creacion de app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import marked from "marked";
+import "./App.css";
 
 function App() {
+  const [markdown, setMarkdown] = useState(`# Markdown Previewer
+
+## Sub-heading
+
+Here's some code, \`<inline style="color:red;">console.log('Hello World')</inline>\`, within a paragraph.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)`);
+
+  const handleMarkdownChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  const getMarkdownText = () => {
+    return { __html: marked(markdown) };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editor-wrapper">
+        <label htmlFor="editor">Editor</label>
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={handleMarkdownChange}
+        />
+      </div>
+      <div className="preview-wrapper">
+        <label htmlFor="preview">Preview</label>
+        <div id="preview" dangerouslySetInnerHTML={getMarkdownText()} />
+      </div>
     </div>
   );
 }
